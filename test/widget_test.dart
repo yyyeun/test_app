35a1a2c9fd6ca5294e2c52e74c57cc6d84194573
yyyeun <yummytomato@naver.com>
2021-01-07 23:37:25 +0900

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:test_app/main.dart';
 
+// test_app
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
